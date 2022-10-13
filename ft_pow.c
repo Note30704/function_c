a6d8ddfe4411194a3a807138693f12b1229bed82
Gyeongnam Kim <rudska252@naver.com>
2022-10-13 19:59:21 +0900

    ft_pow

--- ft_pow.c
+++ ft_pow.c
@@ -2,11 +2,11 @@ double	ft_pow(double x, int n)
 {
 	if (n == 0)
 		return (1);
-	else if (n & 1)		//n % 2 == 1;
+	else if (n & 1)		//n % 2 == 1 (홀수);
 		return (x * ft_pow(x * x, (n - 1) / 2));
 	else
 		return (ft_pow(x * x, n / 2));
 }
 
 
-//n이 홀수 인 경우 (n - 1)로 인해 x를 한번 더 곱해준다.
+//n이 홀수 인 경우 (n - 1)로 인해 x를 한 번 더 곱해준다.
